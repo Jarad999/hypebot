4de19b9954dc616ff09a97157da28017bb781b3f
ThatGuyHyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-08-02 18:44:10 -0700

    Create install.sql

---
+++ schemas/install.sql
@@ -0,0 +1,33 @@
+CREATE TABLE users (
+    userid varchar(255),
+    warns INT,
+    kicks INT,
+    bans INT,
+    mutes INT,
+    isMuted varchar(255),
+    muteTime INT
+);
+
+CREATE TABLE stickymsgs (
+    channelid varchar(255),
+    msg TEXT,
+    enforcerid varchar(255)
+);
+
+CREATE TABLE offlinebans (
+    id varchar(255),
+    reason text
+);
+
+CREATE TABLE chatlvl (
+    userid text,
+    userxp INT,
+    userlvl INT
+);
+
+CREATE TABLE cases (
+    userid varchar(255),
+    reason text,
+    uniqueid varchar(255),
+    enforcerid varchar(255),
+    type varchar(255)

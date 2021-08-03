e1c11c911dbaf30a1510af8d8a545eb497f3215f
ThatGuyHyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-08-02 18:41:40 -0700

    Update loader.js

--- util/loader.js
+++ util/loader.js
@@ -1,11 +1,35 @@
 const fs = require("fs");
+const Importer = require('mysql-import');
 
 module.exports = {
     async loader(client, Hyperz, config, con) {
 
-        // THIS IS WHERE YOU CAN ADD ON TO THE READY EVENT, THIS FILE IS AUTO EXECUTED ON STARTUP
+        if(config.main.autoImportSQL) {
+        // MySQL Auto Importer Lolz
+        try {
+            const importer = new Importer(config.mysql);
 
-        // Technically this was cut off after the free release
+            // New onProgress method, added in version 5.0!
+            importer.onProgress(progress=>{
+            var percent = Math.floor(progress.bytes_processed / progress.total_bytes * 10000) / 100;
+            console.log(`${percent}% Completed`);
+            });
+    
+            importer.import('./schemas/install.sql').then(()=>{
+            var files_imported = importer.getImported();
+            console.log(`${files_imported.length} SQL file(s) imported.`);
+            }).catch(err=>{
+                if(config.main.debugmode) {
+                    console.error(err);
+                }
+            });
+        } catch(e) {
+            if(config.main.debugmode) {
+                console.log(e)
+            }
+        }
+            
+        }
 
     }
 }
